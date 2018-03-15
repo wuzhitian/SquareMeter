7900c8e8fa08b25a83d8a6799f4f37e0882a4870
lihaooo <umbrellahughlee@gmail.com>
2018-03-15 21:10:48 +0800

    Init

---
+++ Limicity/BUSINESS/MODEL/CoreModel/RealNameSafe.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project: Limicity
+ * File: RealNameSafe.php
+ * Create: 2018/3/15
+ * Author: Hugh.Lee
+ * Email: umbrellahughlee@gmail.com
+ * Copyright: SMBC Inc.
+ */
+
+namespace Limicity\BUSINESS\MODEL\CoreModel;
+
+
+class RealNameSafe
+{
+
+}
---
+++ Limicity/BUSINESS/MODEL/Investor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Project: Limicity
+ * File: Investor.php
+ * Create: 2018/3/15
+ * Author: Hugh.Lee
+ * Email: umbrellahughlee@gmail.com
+ * Copyright: SMBC Inc.
+ */
+
+namespace Limicity\BUSINESS\MODEL;
+
+use Limicity\BUSINESS\MODEL\CoreModel\User;
+
+/**
+ * 投资者用户类
+ * Class Investor
+ * @package Limicity\BUSINESS\MODEL
+ */
+class Investor extends User
+{
+
+}
